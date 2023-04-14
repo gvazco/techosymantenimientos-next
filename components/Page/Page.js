086c677328b59dfe808cf35a907ebd6aa53eb55d
gvazco <gustavovazco@gmail.com>
2023-04-14 15:29:44 -0600

    📦 NEW: init repository to TechosyMantenimientos-Mevasa-Next

---
+++ components/Page/Page.js
@@ -0,0 +1,23 @@
+import { BlockRenderer } from "components/BlockRenderer";
+import { MainMenu } from "components/MainMenu";
+import { PageWrapper } from "context/page";
+// import Head from "next/head";
+
+export const Page = (props) => {
+  // console.log("PAGE PROPS: ", props);
+  return (
+    <PageWrapper
+      value={{
+        title: props.title,
+        featuredImage: props.featuredImage,
+      }}
+    >
+      <MainMenu
+        items={props.mainMenuItems}
+        callToActionDestination={props.callToActionDestination}
+        callToActionLabel={props.callToActionLabel}
+      />
+      <BlockRenderer blocks={props.blocks} />
+    </PageWrapper>
+  );
+};
---
+++ pages/[...slug].js
@@ -0,0 +1,54 @@
+import { gql } from "@apollo/client";
+import client from "client";
+import { BlockRenderer } from "components/BlockRenderer";
+import { getPageStaticProps } from "utils/getPageStaticProps";
+import { Page } from "components/Page";
+
+export default Page;
+
+export const getStaticProps = getPageStaticProps;
+
+export const getStaticPaths = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query AllPagesQuery {
+        pages {
+          nodes {
+            uri
+          }
+        }
+        products {
+          nodes {
+            uri
+          }
+        }
+        proyects {
+          nodes {
+            uri
+          }
+        }
+        posts {
+          nodes {
+            uri
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    paths: [
+      ...data.pages.nodes,
+      ...data.products.nodes,
+      ...data.proyects.nodes,
+      ...data.posts.nodes,
+    ]
+      .filter((page) => page.uri !== "/")
+      .map((page) => ({
+        params: {
+          slug: page.uri.substring(1, page.uri.length - 1).split("/"),
+        },
+      })),
+    fallback: false,
+  };
+};
