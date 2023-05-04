48218c9a0be4169b107b559e67fa8e1faffd0c18
Gustavo Vázco <gustavovazco@gmail.com>
2023-05-04 16:33:29 -0600

    Update ProyectSearch.js

--- components/ProyectSearch/ProyectSearch.js
+++ components/ProyectSearch/ProyectSearch.js
@@ -8,7 +8,7 @@ import { Filters } from "./Filters";
 export const ProyectSearch = () => {
   const [proyects, setProyects] = useState([]);
   const [totalResults, setTotalResults] = useState(0);
-  const pageSize = 3;
+  const pageSize = 9;
   const router = useRouter();
 
   const searchProyect = async () => {
